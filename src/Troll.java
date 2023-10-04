98d26e4314d0376c45121e45649036d05ff92344
alessandro.pasqua <alessandro.pasqua@buonarroti.tn.it>
2023-10-04 17:05:13 +0200

    feat: - Added getter and setter methods for hp, dmg and name - Added toString method

--- src/Troll.java
+++ src/Troll.java
@@ -12,4 +12,37 @@ public class Troll {
             throw new Exception("\nValore punti vita non valido!");
         }
     }
+
+    public float getHp() {
+        return hp;
+    }
+
+    public void setHp(float hp) {
+        this.hp = hp;
+    }
+
+    public float getDmg() {
+        return dmg;
+    }
+
+    public void setDmg(float dmg) {
+        this.dmg = dmg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Troll {" +
+                "hp =" + hp +
+                ", dmg =" + dmg +
+                ", name ='" + name + '\'' +
+                '}';
+    }
 }
