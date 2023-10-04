6679494d0c13e9f08d8dc74b9ecdec33b5787ed9
alessandro.pasqua <alessandro.pasqua@buonarroti.tn.it>
2023-10-04 17:03:11 +0200

    feat: - Added Troll constructor method

--- src/Troll.java
+++ src/Troll.java
@@ -2,4 +2,14 @@ public class Troll {
     private float hp;
     private float dmg;
     private String name;
+
+    public Troll(float hp, float dmg, String name) throws Exception {
+        if(hp > 0) {
+            this.hp = hp;
+            this.dmg = dmg;
+            this.name = name;
+        } else {
+            throw new Exception("\nValore punti vita non valido!");
+        }
+    }
 }
