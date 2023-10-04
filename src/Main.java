f862f89581623f4e9cc03b393ff4dbc40099caca
alessandro.pasqua <alessandro.pasqua@buonarroti.tn.it>
2023-10-04 17:17:09 +0200

    test: - Added variable error - Added do-while to instantiate troll1 with user input

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,23 @@ public class Main {
         float hp, dmg;
         String name;
         Scanner input = new Scanner(System.in);
+        boolean error;
 
+        do {
+            try {
+                System.out.print("Inserisci il nome del primo Troll: ");
+                name = input.nextLine();
+                input = new Scanner(System.in);
+                System.out.print("\nInserisci i punti vita del primo Troll: ");
+                hp = input.nextFloat();
+                System.out.print("\nInserisci i punti danno del primo Troll: ");
+                dmg = input.nextFloat();
+                Troll troll1 = new Troll(hp, dmg, name);
+                error = false;
+            } catch (Exception e) {
+                System.out.print(e.getMessage());
+                error = true;
+            }
+        } while(error);
     }
 }
