ca26f41a5ac46572bd289ee1252ff34a9663f59e
alessandro.pasqua <alessandro.pasqua@buonarroti.tn.it>
2023-10-04 17:12:27 +0200

    test: - Added input object

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         float hp, dmg;
         String name;
+        Scanner input = new Scanner(System.in);
+
     }
 }
